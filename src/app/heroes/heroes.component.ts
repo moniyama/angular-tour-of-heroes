f36c0596f2b60ac7f65c4d3d7777c414bee64e0d
Monica <monicasayama@gmail.com>
2023-07-25 10:07:31 -0300

    1 hero component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent {
+  hero = {
+    id: 1,
+    name: "Windsor",
+  }
+}
