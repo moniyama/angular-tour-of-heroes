3506c1c3ab472a047b4f57476ca4a3cb3970a9a0
Monica <monicasayama@gmail.com>
2023-08-04 19:25:14 -0300

    add getHeroes service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -21,7 +21,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule, 
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,14 +3,22 @@ import { HEROESMOCK } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { HeroInterface } from './hero';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
+  private heroesUrl = 'http://localhost:8080/heroes';  // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+      }
+    )
+  };
   getHeroes(): Observable<HeroInterface[]> {
-    const heroes = of(HEROESMOCK);
+    const heroes = this.fetch.get<HeroInterface[]>(this.heroesUrl)
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
@@ -19,5 +27,8 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private fetch: HttpClient,
+    private messageService: MessageService,
+  ) { }
 }
