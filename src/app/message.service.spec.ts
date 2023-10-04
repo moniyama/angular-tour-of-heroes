1b87e8bf76aeb95b3249de53bec7f819fbe4a313
moniyama <monicasayama@gmail.com>
2023-10-03 23:05:51 -0300

    add service message tests

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -6,11 +6,27 @@ describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [MessageService] });
     service = TestBed.inject(MessageService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
+
+  it('should add a message', () => {
+    service.add("deu certo")
+    service.add("deu não")
+    service.add("deu certo de novo")
+    expect(service.messages[0]).toBe("deu certo");
+    expect(service.messages.length).toBe(3);
+  });
+
+  it('should clear the message array', () => {
+    service.add("deu certo")
+    service.add("deu certo de novo")
+    service.clear()
+    expect(service.messages.length).toBe(0);
+    expect(service.messages).toEqual([]);
   });
 });
